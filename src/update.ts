0aaa2c307b7e359a8608e50bd5e50c1dd8a8527c
Brandon! <brandoge91@gmail.com>
2022-04-24 18:39:57 -0400

    okok

--- src/update.ts
+++ src/update.ts
@@ -1,1 +1,13 @@
-const AutoUpdater
+const AutoGitUpdate = require('auto-git-update');
+
+const config = {
+    repository: 'https://github.com/brandoge91/banPanel',
+    fromReleases: false,
+    ignoreFiles: ['src/config.ts'],
+}
+
+const updater = new AutoGitUpdate(config);
+
+
+
+updater.autoUpdate();
