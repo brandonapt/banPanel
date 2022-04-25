863479afa2bcab100f1494f034ca31705488e018
Brandon! <brandoge91@gmail.com>
2022-04-25 07:14:04 -0400

    ok thi WORKS!!!

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,6 @@
 import mongoose, { model, connect } from "mongoose";
 const { Schema } = mongoose;
 import { getIdFromUsername, getUsernameFromId } from "noblox.js";
-// ok i hope thjis works
 
 mongoose.connect('mongodb://localhost:27017/banPanel').catch(console.error);
  const User = model("User", new Schema({
